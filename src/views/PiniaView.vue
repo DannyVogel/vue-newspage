24f14aaafb3eba960574e123090554c189127be2
DannyVogel <danny.hj.vogel@gmail.com>
2023-05-27 15:44:42 +0200

    create pinia route for testing store

---
+++ src/views/PiniaView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {useCounterStore} from '../stores/counter'
+const store = useCounterStore()
+</script>
+
+<template>
+  <main>
+    <h1>Testing Pinia!</h1>
+    <p>Count is {{ store.count }}</p>
+    <p>Double count is {{ store.doubleCount }}</p>
+    <button @click="store.increment">Increment</button>
+  </main>
+</template>
