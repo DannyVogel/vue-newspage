3b73a7c7f025d236814330e3a5a28fbe92b9d458
DannyVogel <danny.hj.vogel@gmail.com>
2023-05-27 15:44:27 +0200

    add route; add basic styles

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,13 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
 
-    <div class="">
+    <div class="w-full h-16 bg-blue-300 flex justify-between items-center px-4">
+      <h1>W.</h1>
 
-      <nav class="">
+      <nav class="w-48 flex justify-around bg-blue-900">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/pinia">Pinia</RouterLink>
       </nav>
     </div>
   </header>
