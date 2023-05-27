8fe43727e6de872f1d175ae4109f63d8aec4ae37
DannyVogel <danny.hj.vogel@gmail.com>
2023-05-27 22:46:27 +0200

    imported header component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <header>
-
-    <div class="w-full h-16 bg-blue-300 flex justify-between items-center px-4">
-      <h1>W.</h1>
-
-      <nav class="w-48 flex justify-around bg-blue-900">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/pinia">Pinia</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <Header />
+    <RouterView />
 </template>
