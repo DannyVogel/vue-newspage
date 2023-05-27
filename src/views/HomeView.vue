93df6e51d778050e3b13bd16b717282f4cbabb38
DannyVogel <danny.hj.vogel@gmail.com>
2023-05-27 22:46:54 +0200

    add content to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,18 @@
 </script>
 
 <template>
-  <main>
-    <h1>This is a home page</h1>
+  <main class="h-screen bg-slate-50 p-4">
+    <div class="flex flex-col items-start gap-4">
+      <img src="../assets/image-web-3-mobile.jpg" alt="web 3 image">
+      <h1 class="text-4xl text-blue-900 font-extrabold">
+        The Bright Future of Web 3.0?
+      </h1>
+      <p class="text-gray-500">
+        We dive into the next evolution of the web that claims to put
+        the power of the platforms back into the hands of the people.
+        But is it really fulfilling its promise?
+      </p>
+      <button class="bg-orange-600 text-sm text-white uppercase tracking-wider px-8 py-2">Read More</button>
+    </div>
   </main>
 </template>
